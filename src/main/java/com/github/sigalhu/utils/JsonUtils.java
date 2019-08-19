a7756074e09c11240a241ea7ee20231b5d401fc5
SigalHu <sigalhu@foxmail.com>
2019-08-19 12:31:53 +0800

    判断 JSON 对象左值和右值是否相等，只比较左值及嵌套的 JSON 对象包含的属性

--- src/main/java/com/github/sigalhu/utils/JsonUtils.java
+++ src/main/java/com/github/sigalhu/utils/JsonUtils.java
@@ -2,8 +2,7 @@ package com.github.sigalhu.utils;
 
 import com.alibaba.fastjson.JSONPath;
 
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 
 /**
  * @author huxujun
@@ -11,6 +10,15 @@ import java.util.Optional;
  */
 public class JsonUtils {
 
+    /**
+     * 获取 JSON Path 下的值
+     *
+     * @param jsonObject
+     * @param path
+     * @param clazz
+     * @param <T>
+     * @return
+     */
     public static <T> Optional<T> parseJsonPath(Object jsonObject, String path, Class<T> clazz) {
         Object result = JSONPath.eval(jsonObject, path);
         if (Objects.isNull(result)) {
@@ -30,5 +38,51 @@ public class JsonUtils {
             return Optional.of(clazz.cast(((Number) result).floatValue()));
         }
         return Optional.of(clazz.cast(result));
+    }
+
+    /**
+     * 判断 JSON 对象左值和右值是否相等，只比较左值及嵌套的 JSON 对象包含的属性
+     *
+     * @param left
+     * @param right
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static boolean leftEquals(Object left, Object right) {
+        if (left == null) {
+            return true;
+        } else if (left instanceof Map) {
+            if (right instanceof Map) {
+                try {
+                    Map<String, Object> lm = (Map<String, Object>) left;
+                    Map<String, Object> rm = (Map<String, Object>) right;
+                    for (Map.Entry<String, Object> entry : lm.entrySet()) {
+                        if (!rm.containsKey(entry.getKey()) || !leftEquals(entry.getValue(), rm.get(entry.getKey()))) {
+                            return false;
+                        }
+                    }
+                    return true;
+                } catch (ClassCastException ex) {
+                    return left.equals(right);
+                }
+            }
+        } else if (left instanceof Collection) {
+            if (right instanceof Collection && ((Collection) left).size() == ((Collection) right).size()) {
+                for (Iterator liter = ((Collection) left).iterator(), riter = ((Collection) right).iterator();
+                     liter.hasNext() && riter.hasNext(); ) {
+                    if (!leftEquals(liter.next(), riter.next())) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        } else if (left.getClass().isArray()) {
+            if (right.getClass().isArray()) {
+                return leftEquals(ArrayUtils.asList(left), ArrayUtils.asList(right));
+            }
+        } else {
+            return left.equals(right);
+        }
+        return false;
     }
 }
---
+++ src/test/java/com/github/sigalhu/utils/JsonUtilsTest.java
@@ -0,0 +1,22 @@
+package com.github.sigalhu.utils;
+
+import com.alibaba.fastjson.JSON;
+import org.junit.Test;
+
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author huxujun
+ * @date 2019-08-18
+ */
+public class JsonUtilsTest {
+
+    @Test
+    public void leftEquals() {
+        String left = "{\"height\":1.75,\"id\":1,\"name\":\"student\",\"phone\":\"01234567\",\"transcript\":[{\"name\":\"mathematics\"},{\"name\":\"physics\"},{\"name\":\"organism\"},{\"name\":\"geography\"}],\"weight\":65.5}";
+        String right = "{\"age\":18,\"height\":1.75,\"id\":1,\"name\":\"student\",\"phone\":\"01234567\",\"transcript\":[{\"name\":\"mathematics\",\"score\":100},{\"name\":\"physics\",\"score\":99},{\"name\":\"organism\",\"score\":98},{\"name\":\"geography\",\"score\":97}],\"weight\":65.5}";
+        assertTrue(JsonUtils.leftEquals(JSON.parseObject(left), JSON.parseObject(right)));
+    }
+}
